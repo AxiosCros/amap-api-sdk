38490ce5e8135414303620e7ca748605f6b2db4d
axios <axioscros@aliyun.com>
2018-03-28 14:26:12 +0800

    AMapOption:CLOUD_MAP_REQUEST_BASE_URL and REST_API_REQUEST_BASE_URL

---
+++ library/AMapOption.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author: axios
+ *
+ * @email: axiosleo@foxmail.com
+ * @blog:  http://hanxv.cn
+ * @datetime: 2018/3/28 14:01
+ */
+
+namespace amap\sdk;
+
+
+class AMapOption
+{
+    const CLOUD_MAP_REQUEST_BASE_URL = "yuntuapi.amap.com/";
+
+    const REST_API_REQUEST_BASE_URL = "restapi.amap.com/";
+}
