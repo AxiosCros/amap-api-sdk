dcfca7826c01ac8c4299a47fcd0afc5b2850a91f
axios <axioscros@aliyun.com>
2018-03-28 17:33:28 +0800

    Request method const

--- library/AMapOption.php
+++ library/AMapOption.php
@@ -15,4 +15,15 @@ class AMapOption
     const CLOUD_MAP_REQUEST_BASE_URL = "yuntuapi.amap.com/";
 
     const REST_API_REQUEST_BASE_URL = "restapi.amap.com/";
+
+    const GET    = 'GET';
+    const PUT    = 'PUT';
+    const POST   = 'POST';
+    const HEAD   = 'HEAD';
+    const DELETE = 'DELETE';
+    const HEADER = 'HEADER';
+    const TRACE  = 'TRACE';
+    const PATCH  = 'PATCH';
+    const CONNECT= 'CONNECT';
+    const OPTIONS= 'OPTIONS';
 }
