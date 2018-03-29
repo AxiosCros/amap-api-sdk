4fe64582043112e96e3dd36ae39bee583b1f8f93
axios <axioscros@aliyun.com>
2018-03-29 13:23:26 +0800

    debug namespace error

--- library/core/traits/ClientTrait.php
+++ library/core/traits/ClientTrait.php
@@ -9,7 +9,6 @@
 
 namespace amap\sdk\core\traits;
 
-
 use amap\sdk\core\AMapException;
 
 trait ClientTrait
@@ -19,11 +18,14 @@ trait ClientTrait
      * @param $arguments
      * @return mixed
      * @throws AMapException
+     * @throws \ReflectionException
      */
     public static function __callStatic($name, $arguments)
     {
         $class_name = ucfirst(strtolower($name));
-        $class_name = __NAMESPACE__ . '\\request\\'.$class_name;
+        $Reflection = new \ReflectionClass(get_called_class());
+        $namespace = $Reflection->getNamespaceName();
+        $class_name = $namespace . '\\request\\'.$class_name;
         if(!class_exists($class_name)){
             throw new AMapException("Class not exist");
         }
